4e9ce2f1eef13ce5a61b4e92977938af022aeb8f
Stephane Point <point.steph@gmail.com>
2021-08-02 18:20:35 +0200

    new MA env dev

--- init.sh
+++ init.sh
@@ -39,7 +39,7 @@ main() {
   [[ -f "${HOME}/.psqlrc" ]] && rm ~/.psqlrc
 
   local sys=$OSTYPE; shift
-  for item in zsh httpie tmux; do
+  for item in zsh httpie tmux gitlint; do
     print_title "Installing $item"
     if [[ "$sys" == "linux"* ]]; then
       sudo apt -y install $item
