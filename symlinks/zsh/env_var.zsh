31b40172cb244ab783b6e2c4d1bcf95458002753
Stephane Point <point.steph@gmail.com>
2023-01-14 00:06:26 +0100

    edit zshrc aliases and add env vars

---
+++ symlinks/zsh/env_var.zsh
@@ -0,0 +1,8 @@
+# password store
+export PASSWORD_STORE_DIR=${HOME}/my_password_store
+# gpg
+export GPG_TTY=$(tty)
+# python
+export PATH=$(pyenv root)/shims:$PATH
+# node
+export PATH="/usr/local/bin:$PATH"
