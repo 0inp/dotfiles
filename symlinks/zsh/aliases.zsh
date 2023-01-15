13a6a187e1e8d7876e6f64faf77be912dc5f0f28
Stephane Point <point.steph@gmail.com>
2023-01-15 20:54:18 +0100

    Clean zsh config and add zinit plugins

--- symlinks/zsh/aliases.zsh
+++ symlinks/zsh/aliases.zsh
@@ -1,6 +1,48 @@
+## check if given command exists on the system
+command_exists() { (( $+commands[$1] )) }
+
+
 alias less='less -r'
 alias vi='nvim'
 alias tmux='tmux -2'
 alias python=python3
 
 alias fixssh='eval $(tmux showenv -s SSH_AUTH_SOCK)'
+
+# ls => exa
+
+# exchange ls with exa
+# https://the.exa.website/
+command_exists "exa"
+if [ "${?}" -eq "0" ]; then
+  alias ls='exa'
+else
+  # if no exa can be used make ls colorful
+  if [ "${OSTYPE}" = "linux-gnu" ]; then
+    alias ls='ls --color=auto'
+  elif [ "${OSTYPE}" = "darwin" ]; then
+    export CLICOLOR=1
+    export LSCOLORS=GxFxCxDxBxegedabagaced
+  fi
+fi
+
+# options
+alias lsa='ls -lah'
+alias l='ls -la'
+alias ll='ls -l'
+
+# grep => ripgrep
+
+# exchange grep with ripgrep
+# https://github.com/BurntSushi/ripgrep
+command_exists "rg"
+if [ "${?}" -eq "0" ]; then
+  alias grep='rg'
+fi
+
+# neovim
+command_exists "nvim"
+if [ "${?}" -eq "0" ]; then
+  alias vim=nvim
+  alias nv=nvim
+fi
---
+++ symlinks/zsh/zinit.zsh
@@ -0,0 +1,43 @@
+###################################################################################
+# zinit (plugin manager)
+###################################################################################
+### Added by Zinit's installer
+if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
+    print -P "%F{33}▓▒░ %F{220}Installing DHARMA Initiative Plugin Manager (zdharma/zinit)…%f"
+    command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
+    command git clone https://github.com/zdharma-continuum/zinit.git "$HOME/.zinit/bin" && \
+        print -P "%F{33}▓▒░ %F{34}Installation successful.%f" || \
+        print -P "%F{160}▓▒░ The clone has failed.%f"
+fi
+source "$HOME/.zinit/bin/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+### End of Zinit installer's chunk
+
+
+
+###################################################################################
+# load plugins
+###################################################################################
+zinit light zdharma-continuum/fast-syntax-highlighting
+
+zinit wait lucid light-mode for \
+  atload"_zsh_autosuggest_start" \
+      zsh-users/zsh-autosuggestions \
+  blockf atpull'zinit creinstall -q .' \
+      zsh-users/zsh-completions
+
+zinit light mfaerevaag/wd
+zinit light ael-code/zsh-colored-man-pages
+zinit light wfxr/forgit
+
+###################################################################################
+# finish all
+###################################################################################
+autoload -Uz compinit
+compinit
+
+zinit cdreplay -q
+
+# profiling (comment in to profile zsh)
+#zprof
