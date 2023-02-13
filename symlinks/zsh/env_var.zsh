64a02ffc45792edcff716bddbf07d4f92421715b
Stephane Point <point.steph@gmail.com>
2023-02-12 22:45:22 -0500

    Add poetry env var in PATH

--- symlinks/zsh/env_var.zsh
+++ symlinks/zsh/env_var.zsh
@@ -4,5 +4,7 @@ export PASSWORD_STORE_DIR=${HOME}/my_password_store
 export GPG_TTY=$(tty)
 # python
 export PATH=$(pyenv root)/shims:$PATH
+#poetry
+export PATH=${HOME}/.local/bin:$PATH
 # node
 export PATH="/usr/local/bin:$PATH"
