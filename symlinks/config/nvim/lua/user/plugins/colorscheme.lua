a69b05c4fc9da6270aa9f4da5402d2e0b098a557
Stephane Point <point.steph@gmail.com>
2023-03-05 12:40:50 -0500

    Add rose-pine nvim colorscheme

--- symlinks/config/nvim/lua/user/plugins/colorscheme.lua
+++ symlinks/config/nvim/lua/user/plugins/colorscheme.lua
@@ -3,31 +3,43 @@
 -- url  : https://github.com/navarasu/onedark.nvim
 -------------------------------------------------
 local M = {
-  "navarasu/onedark.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = function()
-		local colors = require("user.utils").git_colors
-		return {
-			style = "cool",
-			-- hide_inactive_statusline = true,
-			on_highlights = function(hl, c)
-				hl.GitSignsAdd = {
-					fg = colors.GitAdd,
-				}
-				hl.GitSignsChange = {
-					fg = colors.GitChange,
-				}
-				hl.GitSignsDelete = {
-					fg = colors.GitDelete,
-				}
-			end,
-		}
-	end,
-	config = function(_, opts)
-		local onedark = require("onedark")
-		onedark.setup(opts)
-		onedark.load()
-	end,
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = function()
+      local colors = require("user.utils").git_colors
+      return {
+        style = "cool",
+        -- hide_inactive_statusline = true,
+        on_highlights = function(hl, c)
+          hl.GitSignsAdd = {
+            fg = colors.GitAdd,
+          }
+          hl.GitSignsChange = {
+            fg = colors.GitChange,
+          }
+          hl.GitSignsDelete = {
+            fg = colors.GitDelete,
+          }
+        end,
+      }
+    end,
+    config = function(_, opts)
+      local onedark = require("onedark")
+      onedark.setup(opts)
+      onedark.load()
+    end,
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = true,
+    priority = 1000,
+    config = function()
+      require("rose-pine").setup()
+      vim.cmd("colorscheme rose-pine")
+    end,
+  },
 }
 return M
