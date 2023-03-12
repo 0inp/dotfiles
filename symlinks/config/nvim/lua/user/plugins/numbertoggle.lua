3f6f1d49711ec6e640fbc4b70c21fb025a38b594
Stephane Point <point.steph@gmail.com>
2023-03-12 18:23:37 -0500

    Use AstroNvim

--- symlinks/config/nvim/lua/user/plugins/numbertoggle.lua
+++
@@ -1,9 +0,0 @@
--------------------------------------------------
--- name : numbertoggle
--- url  : https://github.com/sitiom/nvim-numbertoggle
--------------------------------------------------
-local M = {
-  "sitiom/nvim-numbertoggle",
-}
-
-return M
