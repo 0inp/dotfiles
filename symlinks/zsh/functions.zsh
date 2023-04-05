b934f010a8c1e338105ca06f614d4b2df0cf7aa1
Stephane Point <point.steph@gmail.com>
2023-04-05 17:47:31 -0500

    Clean zsh functions

--- symlinks/zsh/functions.zsh
+++ symlinks/zsh/functions.zsh
@@ -8,41 +8,16 @@ function timezsh() {
   for i in $(seq 1 10); do /usr/bin/time $shell -i -c exit; done
 }
 
-# function path_remove() {
-#   PATH=$(echo -n "$PATH" | awk -v RS=: -v ORS=: "\$0 != \"$1\"" | sed 's/:$//')
-# }
-#
-# function path_append() {
-#   path_remove "$1"
-#   PATH="${PATH:+"$PATH:"}$1"
-# }
-#
-# function path_prepend() {
-#   path_remove "$1"
-#   PATH="$1${PATH:+":$PATH"}"
-# }
-#
-# function here() {
-#   local loc
-#   if [ "$#" -eq 1 ]; then
-#     loc=$(realpath "$1")
-#   else
-#     loc=$(realpath ".")
-#   fi
-#   ln -sfn "${loc}" "$HOME/.shell.here"
-#   echo "here -> $(readlink $HOME/.shell.here)"
-# }
-#
-# there="$HOME/.shell.here"
-#
-# function there() {
-#   cd "$(readlink "${there}")"
-# }
-
-function dfu() {
-  cd ~/.dotfiles && git pull --ff-only && ./install -q
+function path_remove() {
+  PATH=$(echo -n "$PATH" | awk -v RS=: -v ORS=: "\$0 != \"$1\"" | sed 's/:$//')
 }
 
-function wd() {
-  . /Users/oinp/bin/wd/wd.sh
+function path_append() {
+  path_remove "$1"
+  PATH="${PATH:+"$PATH:"}$1"
+}
+
+function path_prepend() {
+  path_remove "$1"
+  PATH="$1${PATH:+":$PATH"}"
 }
