a116ff123e3218e0ed1200de1b2713c533cc65b5
Stephane Point <point.steph@gmail.com>
2024-05-11 20:55:15 -0500

    feat(nvim): Ensure LSP servers installed

--- symlinks/config/nvim/init.lua
+++ symlinks/config/nvim/init.lua
@@ -578,6 +578,11 @@ require('lazy').setup({
             },
           },
         },
+        tsserver = {},
+        prettier = {},
+        eslint = {},
+        isort = {},
+        black = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -622,11 +627,11 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettierd', 'prettier' } },
       },
     },
   },
