0123176f32079e0572281d5dabeb492f8332512a
Stephane Point <point.steph@gmail.com>
2024-05-28 13:04:26 -0500

    chore(nvim): reorganization of config fil structure from kickstart config

---
+++ symlinks/config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,18 @@
+return {
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+
+  -- Highlight todo, notes, etc in comments
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  -- See `:help gitsigns` to understand what the configuration keys do
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end
+  },
+}
---
+++ symlinks/config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,103 @@
+local statusline_config = function()
+  local mini_statusline = require("mini.statusline")
+  mini_statusline.setup({
+    use_icons = true,
+    content = {
+      active = function()
+      local m = require("mini.statusline")
+
+      local fnamemodify = vim.fn.fnamemodify
+
+      local project_name = function()
+          local current_project_folder = fnamemodify(vim.fn.getcwd(), ":t")
+          local parent_project_folder = fnamemodify(vim.fn.getcwd(), ":h:t")
+          return parent_project_folder .. "/" .. current_project_folder
+      end
+
+      local lazy_plug_count = function()
+          local stats = require("lazy").stats()
+          return " " .. stats.count
+      end
+
+      local lazy_startup = function()
+          local stats = require("lazy").stats()
+          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+          return " " .. ms .. "ms"
+      end
+
+      local lazy_updates = function()
+          return require("lazy.status").updates()
+      end
+
+      -- local wtf = function()
+      --     return require("wtf").get_status()
+      -- end
+
+      local auto_format_indicator = function()
+          if vim.g.vin_autoformat_enabled then
+              return "󰉶 On"
+          else
+              return "󰉶 Off"
+          end
+      end
+
+      local mode, mode_hl = m.section_mode({ trunc_width = 120 })
+
+      local spell = vim.wo.spell and (m.is_truncated(120) and "S" or "SPELL") or ""
+      local wrap = vim.wo.wrap and (m.is_truncated(120) and "W" or "WRAP") or ""
+      local git = m.section_git({ trunc_width = 75 })
+      local diagnostics = m.section_diagnostics({ trunc_width = 75 })
+      local searchcount = m.section_searchcount({ trunc_width = 75 })
+      local location = m.section_location({ trunc_width = 120 })
+      local fileinfo = m.section_fileinfo({ trunc_width = 125 })
+      local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+      local colorscheme_name = vim.g.colors_name or "default"
+      local colorscheme = m.is_truncated(200) and "" or " " .. colorscheme_name
+
+      return m.combine_groups({
+          { hl = mode_hl, strings = { mode } },
+          {
+              hl = "Function",
+              strings = (m.is_truncated(250) and { git } or { project_name(), git }),
+          },
+
+          "%<", -- Mark general truncate point
+
+          { hl = "Comment", strings = { filename, location, diagnostics } },
+
+          "%=", -- End left alignment
+
+          { hl = "Comment", strings = { searchcount } },
+          {
+              hl = "Comment",
+              strings = (m.is_truncated(120) and {} or {
+                  -- wtf(),
+                  wrap,
+                  spell,
+                  auto_format_indicator(),
+                  lazy_plug_count(),
+                  lazy_updates(),
+                  lazy_startup(),
+                  colorscheme,
+              }),
+          },
+          { hl = mode_hl, strings = { fileinfo } },
+      })
+      end,
+                },
+    set_vim_settings = false,
+  })
+end
+
+return {
+  { 'echasnovski/mini.ai', version = false },
+  { 'echasnovski/mini.pairs', version = false },
+  { 'echasnovski/mini.starter', version = false },
+  { 'echasnovski/mini.surround', version = false },
+  {
+    'echasnovski/mini.statusline',
+    version = false,
+    event = "VeryLazy",
+    config = statusline_config
+  },
+}
