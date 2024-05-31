1fb91abe645924170c5ab7c63d3740f57eee469c
Stephane Point <point.steph@gmail.com>
2024-05-30 19:15:46 -0500

    feat(nvim): better completion with snippets

--- symlinks/config/nvim/lua/plugins/completion.lua
+++ symlinks/config/nvim/lua/plugins/completion.lua
@@ -1,24 +1,32 @@
 return {
   'hrsh7th/nvim-cmp',
-  lazy = false,
-  priority = 100,
+  event = 'InsertEnter',
   dependencies = {
+    'neovim/nvim-lspconfig',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-cmdline',
+    'rafamadriz/friendly-snippets',
     { 'L3MON4D3/LuaSnip', build = 'make install_jsregexp' },
     'saadparwaiz1/cmp_luasnip',
   },
   config = function()
     -- And you can configure cmp even more, if you want to.
     local cmp = require 'cmp'
+    local luasnip = require 'luasnip'
+    require('luasnip.loaders.from_vscode').lazy_load()
+    luasnip.config.setup {}
 
     cmp.setup {
-      sources = {
-        { name = 'nvim_lsp' },
-        { name = 'cody' },
-        { name = 'path' },
-        { name = 'buffer' },
+      snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      completion = {
+        completeopt = 'menu,menuone,noinsert',
       },
       mapping = cmp.mapping.preset.insert {
         ['<C-Space>'] = cmp.mapping.complete(),
@@ -34,6 +42,35 @@ return {
           { 'i', 'c' }
         ),
       },
+      sources = cmp.config.sources {
+        { name = 'nvim_lsp' }, -- lsp
+        { name = 'luasnip' }, -- snippets
+        { name = 'buffer' }, -- text within current buffer
+        { name = 'path' }, -- file system paths),
+      },
+      window = {
+        -- Add borders to completions popups
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
     }
+    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline({ '/', '?' }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = 'buffer' },
+      },
+    })
+
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path' },
+      }, {
+        { name = 'cmdline' },
+      }),
+      matching = { disallow_symbol_nonprefix_matching = false },
+    })
   end,
 }
