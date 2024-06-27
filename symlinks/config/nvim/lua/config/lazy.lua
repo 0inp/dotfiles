fc8af211ae043ac88db0de345e33235f4ee19c92
Stephane Point <point.steph@gmail.com>
2024-06-26 21:03:01 -0500

    feat(nvim): add lazyvim dotfiles extras

--- symlinks/config/nvim/lua/config/lazy.lua
+++ symlinks/config/nvim/lua/config/lazy.lua
@@ -37,6 +37,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.ui.mini-starter" },
+    { import = "lazyvim.plugins.extras.util.dot" },
 
     -- import/override with your plugins
     { import = "plugins" },
