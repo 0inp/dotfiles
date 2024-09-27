29e0b0277113d3bc0b88557dc186391980fc9649
Stephane Point <point.steph@gmail.com>
2024-09-27 09:45:01 -0500

    feat(nvim): change neotree config and cleanup

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -11,4 +11,24 @@ return {
       },
     }
   end,
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".git",
+            ".DS_Store",
+            "thumbs.db",
+            ".github",
+            ".gitignore",
+            "package-lock.json",
+          },
+          never_show = {},
+        },
+      },
+    })
+  end,
 }
