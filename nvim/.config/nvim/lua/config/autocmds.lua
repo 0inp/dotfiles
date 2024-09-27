ce32a27cd4f64a0c4cb041cafa80bcb69e63dd85
Stephane Point <point.steph@gmail.com>
2024-09-27 09:43:44 -0500

    feat(nvim): new lazy update autocmd

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -9,3 +9,16 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.formatoptions:remove({ "r", "o" })
   end,
 })
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = augroup("autoupdate"),
+  callback = function()
+    require("lazy").update({
+      show = false,
+    })
+  end,
+})
