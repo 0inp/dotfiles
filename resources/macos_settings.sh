04827700c0cb764fbd381d4966134aef1683fa94
Stephane Point <point.steph@gmail.com>
2025-04-15 17:36:40 -0500

    feat(macos): Add macos setting to check updates daily

--- resources/macos_settings.sh
+++ resources/macos_settings.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+echo "show the ~/Library folder in Finder"
+chflags nohidden ~/Library
+
 echo "Enable Quit Menu item on finder"
 defaults write com.apple.Finder QuitMenuItem 1
 killall -HUP Finder
+
+# Check for software updates daily, not just once per week
+echo "Software updates checking daily"
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
 echo "Finder: show all filename extensions"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
@@ -14,9 +21,6 @@ defaults write com.apple.terminal StringEncodings -array 4
 
 echo "expand save dialog by default"
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-
-echo "show the ~/Library folder in Finder"
-chflags nohidden ~/Library
 
 echo "Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
@@ -48,6 +52,20 @@ defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool
 echo "Enable Safari’s debug menu"
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 
-echo "Kill affected applications"
+# SSD specifics
+# Remove the sleep image file to save disk space
+sudo rm /private/var/vm/sleepimage
 
+# Create a zero-byte file instead…
+sudo touch /private/var/vm/sleepimage
+
+# …and make sure it can’t be rewritten
+sudo chflags uchg /private/var/vm/sleepimage
+echo "SSD optimized."
+
+# Disable the sudden motion sensor as it’s not useful for SSDs
+sudo pmset -a sms 0
+echo "Sudden motion sensor disabled (SSDs not needed)."
+
+echo "Kill affected applications"
 for app in Safari Finder Dock Mail SystemUIServer; do killall "$app" >/dev/null 2>&1; done
