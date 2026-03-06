dc2f5c5766e1f9aef878d86f52373cfc754b0c66
Stephane Point <point-stephane@proton.me>
2026-03-06 16:04:33 -0500

    feat(zsh): improve zsh completion caching

--- zsh/.config/zsh/completion.zsh
+++ zsh/.config/zsh/completion.zsh
@@ -1,11 +1,8 @@
-autoload -U compinit; compinit
-_comp_options+=(globdots) # With hidden files
-
 # +---------+
 # | General |
 # +---------+
 
-# Use hjlk in menu selection (during completion)
+# Use hjkl in menu selection (during completion)
 # Doesn't work well with interactive mode
 bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'k' vi-up-line-or-history
@@ -31,7 +28,7 @@ setopt COMPLETE_IN_WORD     # Complete from both ends of a word.
 # | zstyles |
 # +---------+
 
-# Ztyle pattern
+# Zstyle pattern
 # :completion:<function>:<completer>:<command>:<argument>:<tag>
 
 # Define completers
@@ -47,8 +44,6 @@ zle -C alias-expension complete-word _generic
 bindkey '^Xa' alias-expension
 zstyle ':completion:alias-expension:*' completer _expand_alias
 
-# Use cache for commands which use it
-
 # Allow you to select in a menu
 zstyle ':completion:*' menu select
 
@@ -57,18 +52,15 @@ zstyle ':completion:*' complete-options true
 
 zstyle ':completion:*' file-sort modification
 
-
+# Colors and messages
 zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
 zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %D %d --%f'
 zstyle ':completion:*:*:*:*:messages' format ' %F{purple} -- %d --%f'
 zstyle ':completion:*:*:*:*:warnings' format ' %F{red}-- no matches found --%f'
-# zstyle ':completion:*:default' list-prompt '%S%M matches%s'
-# Colors for files and directory
-zstyle ':completion:*:*:*:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
 # Only display some tags for the command cd
 zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
-# zstyle ':completion:*:complete:git:argument-1:' tag-order !aliases
 
 # Required for completion to be in good groups (named after the tags)
 zstyle ':completion:*' group-name ''
@@ -80,4 +72,24 @@ zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 
 
 zstyle ':completion:*' keep-prefix true
 
+# SSH hosts completion
 zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
+
+# Include hidden files in completion
+_comp_options+=(globdots)
+
+# +------------------+
+# | Completion Init  |
+# +------------------+
+
+# Skip global compdefs for faster startup
+skip_global_compinit=1
+
+# Load and precompile completions
+autoload -U compinit
+if [[ -n "$XDG_CACHE_HOME/zsh/.zcompcache"(*.zwc)(N.m+1) ]]; then
+  compinit -C -d "$XDG_CACHE_HOME/zsh/.zcompcache"
+else
+  compinit -d "$XDG_CACHE_HOME/zsh/.zcompcache"
+fi
+
