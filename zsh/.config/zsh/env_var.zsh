48afb103237b277d4da74e627bf6d034cd2c8e79
Stephane Point <point-stephane@proton.me>
2026-03-06 22:01:48 -0500

    fix(nvm): fix nvm installation

--- zsh/.config/zsh/env_var.zsh
+++ zsh/.config/zsh/env_var.zsh
@@ -27,8 +27,8 @@ export PATH=${HOME}/.local/bin:$PATH
 # node
 export PATH="/usr/local/bin:$PATH"
 export NVM_DIR="$HOME/.nvm"
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Use vim as the editor
 export PATH="$PATH:/opt/nvim/"
