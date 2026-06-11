17d8237f98b9c1a03d0bb1c3bbdb2e2fcf11a269
Stephane Point <point-stephane@proton.me>
2026-06-11 12:07:32 -0500

    feat(nvim): update minifiles keymaps with toggling dotfiles, bookmarks, and splits

--- nvim/.config/nvim/lua/plugins/base.lua
+++ nvim/.config/nvim/lua/plugins/base.lua
@@ -24,6 +24,74 @@ vim.keymap.set("n", "<leader>-", function()
 	MiniFiles.reveal_cwd()
 end, { desc = "Toggle into currently opened file" })
 
+local show_dotfiles = true
+
+local filter_show = function(fs_entry)
+	return true
+end
+
+local filter_hide = function(fs_entry)
+	return not vim.startswith(fs_entry.name, ".")
+end
+
+local toggle_dotfiles = function()
+	show_dotfiles = not show_dotfiles
+	local new_filter = show_dotfiles and filter_show or filter_hide
+	MiniFiles.refresh({ content = { filter = new_filter } })
+end
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "MiniFilesBufferCreate",
+	callback = function(args)
+		local buf_id = args.data.buf_id
+		-- Tweak left-hand side of mapping to your liking
+		vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
+	end,
+})
+
+local map_split = function(buf_id, lhs, direction)
+	local rhs = function()
+		-- Make new window and set it as target
+		local cur_target = MiniFiles.get_explorer_state().target_window
+		local new_target = vim.api.nvim_win_call(cur_target, function()
+			vim.cmd(direction .. " split")
+			return vim.api.nvim_get_current_win()
+		end)
+
+		MiniFiles.set_target_window(new_target)
+
+		-- This intentionally doesn't act on file under cursor in favor of
+		-- explicit "go in" action (`l` / `L`). To immediately open file,
+		-- add appropriate `MiniFiles.go_in()` call instead of this comment.
+	end
+
+	-- Adding `desc` will result into `show_help` entries
+	local desc = "Split " .. direction
+	vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+end
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "MiniFilesBufferCreate",
+	callback = function(args)
+		local buf_id = args.data.buf_id
+		-- Tweak keys to your liking
+		map_split(buf_id, "<C-s>", "belowright horizontal")
+		map_split(buf_id, "<C-v>", "belowright vertical")
+		map_split(buf_id, "<C-t>", "tab")
+	end,
+})
+
+local set_mark = function(id, path, desc)
+	MiniFiles.set_bookmark(id, path, { desc = desc })
+end
+vim.api.nvim_create_autocmd("User", {
+	pattern = "MiniFilesExplorerOpen",
+	callback = function()
+		set_mark("d", "~/dotfiles", "Dotfiles")
+		set_mark("~", "~", "Home directory")
+	end,
+})
+
 ---- mini notify ----
 require("mini.notify").setup({
 	-- only show messages
