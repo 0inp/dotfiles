ce63085da2dd599851ec97ffe74991fc59312793
Stephane Point <point-stephane@proton.me>
2026-06-20 08:27:09 -0500

    feat(nvim): add markdown-preview

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -2,3 +2,15 @@ vim.pack.add({
 	"https://github.com/yousefhadder/markdown-plus.nvim",
 })
 require("markdown-plus").setup()
+vim.pack.add({
+	"https://github.com/selimacerbas/live-server.nvim",
+	"https://github.com/selimacerbas/markdown-preview.nvim",
+})
+require("markdown_preview").setup({
+	-- all optional; sane defaults shown
+	instance_mode = "takeover", -- "takeover" (one tab) or "multi" (tab per instance)
+	port = 0, -- 0 = auto (8421 for takeover, OS-assigned for multi)
+	open_browser = true,
+	default_theme = "dark", -- "dark" or "light"; initial preview theme
+	debounce_ms = 300,
+})
